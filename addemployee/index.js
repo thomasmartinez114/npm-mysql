5ff8e99dc0aba2982acfd9774b3a68277131c1db
Thomas Martinez <tmartinez114@gmail.com>
2024-05-16 21:17:26 -0400

    form inserting submission to db

--- addemployee/index.js
+++ addemployee/index.js
@@ -2,10 +2,28 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const mysql = require("mysql")
+const bodyParser = require("body-parser")
 
 // dotenv
 require("dotenv").config()
 const PORT = process.env.PORT
+
+//   MySQL Connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  tableName: process.env.DB_TABLE_NAME,
+})
+
+connection.connect((err) => {
+  if (err) throw err
+  console.log("Connected to MySQL Server!")
+})
+
+// Middleware to parse JSON Bodies
+app.use(bodyParser.json())
 
 // Set the view engine to EJS
 app.set("view engine", "ejs")
@@ -22,24 +40,43 @@ app.get("/addNew", (req, res) => {
   res.render("addNew")
 })
 
-app.listen(PORT, () => {
-  console.log(`Node.js server running at http://localhost:${PORT}`)
-  //   console.log(`add user to database at http://localhost:${PORT}/addemployee`)
+// Define API Endpoint
+app.post("/addEmployee", (req, res) => {
+  const {
+    FirstName,
+    LastName,
+    Department,
+    JobTitle,
+    StartDate,
+    EndDate,
+    Salary,
+    Email,
+  } = req.body
+
+  // Query to create new user
+  connection.query(
+    `INSERT INTO ${process.env.DB_TABLE_NAME} SET ?`,
+    {
+      FirstName,
+      LastName,
+      Department,
+      JobTitle,
+      StartDate,
+      EndDate,
+      Salary,
+      Email,
+    },
+    (err) => {
+      if (err) throw err
+      console.log("1 record inserted")
+      res.send("Employee added successfully")
+    }
+  )
 })
 
-//   //   MySQL Connection
-//   const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     tableName: process.env.DB_TABLE_NAME,
-//   })
-
-//   connection.connect((err) => {
-//     if (err) throw err
-//     console.log("Connected to MySQL Server!")
-//   })
+app.listen(PORT, () => {
+  console.log(`Node.js server running at http://localhost:${PORT}`)
+})
 
 //   // Employee Object
 //   const employee = {
@@ -62,20 +99,6 @@ app.listen(PORT, () => {
 //     EndDate,
 //     Salary,
 //   } = employee
-
-// Query to create new user
-//   connection.query(
-//     `INSERT INTO ${process.env.DB_TABLE_NAME} SET ?`,
-//     { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary },
-//     (err) => {
-//       if (err) throw err
-//       console.log("1 record inserted")
-//       res.send("Employee added successfully")
-
-//       // res.redirect('/listemployee'); // Redirect to list employees after adding
-//     }
-//   )
-// })
 
 // app.post('/addemployee', (req, res) => {
 // const { FirstName, LastName, Department, JobTitle, StartDate, EndDate, Salary } = employee;
--- addemployee/public/js/script.js
+++ addemployee/public/js/script.js
@@ -1,4 +1,44 @@
-createEmployee = () => {
-  event.preventDefault()
-  console.log("Javascript file")
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("employeeForm")
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    let FirstName = document.getElementById("FirstName").value
+    let LastName = document.getElementById("LastName").value
+    let Email = document.getElementById("Email").value
+    let Department = document.getElementById("Department").value
+    let JobTitle = document.getElementById("JobTitle").value
+    let Salary = document.getElementById("Salary").value
+    let StartDate = document.getElementById("StartDate").value
+    let EndDate = document.getElementById("EndDate").value
+
+    try {
+      const response = await fetch("/addEmployee", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          FirstName,
+          LastName,
+          Email,
+          Department,
+          JobTitle,
+          Salary,
+          StartDate,
+          EndDate,
+        }),
+      })
+
+      if (response.ok) {
+        const result = await response.json()
+        console.log("Data inserted successfully. ID: " + result.insertId)
+      } else {
+        console.log("Error inserting data.")
+      }
+    } catch (error) {
+      //   messageElement.textContent = "Error: " + error.message
+      console.log(error.message)
+    }
+  })
+})
